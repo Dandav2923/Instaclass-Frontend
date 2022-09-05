94f41365e4c69ca489e88b06d887b6f33af03a6c
furvietto <fulvio-94@hotmail.it>
2022-09-05 11:39:09 +0200

    add login service and post

--- src/app/logininstitute/logininstitute.component.ts
+++ src/app/logininstitute/logininstitute.component.ts
@@ -1,3 +1,5 @@
+import { LoginInstituteService } from './../service/login-institute.service';
+import { Login } from './../model/login';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -10,7 +12,8 @@ export class LogininstituteComponent implements OnInit {
 
   form:FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private loginInstituteService: LoginInstituteService) {
     this.form = this.fb.group({
       username:this.fb.control("",[Validators.required, Validators.minLength(4)]),
       password:this.fb.control("",[Validators.required, Validators.minLength(3), Validators.maxLength(15)] )
@@ -21,8 +24,13 @@ export class LogininstituteComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.form.getRawValue());
-
+    if (this.form.valid) {
+      let login: Login = {
+        username: this.form.get('username')?.value,
+        password: this.form.get('password')?.value,
+      }
+      this.loginInstituteService.logInstitute(login).subscribe
+    }
   }
 
 }
---
+++ src/app/service/login-institute.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from '../model/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginInstituteService {
+
+  constructor(private http: HttpClient) { }
+
+  logInstitute(login:Login) {
+    return this.http.post("http://localhost:9191/institute/v1/institutes/loginInstitute",login)
+  }
+}
